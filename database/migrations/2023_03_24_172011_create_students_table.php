6e082fe3e42cf4d44f3a7a709efa8ae9c793ff2b
leoc0104 <leocamargo0104@gmail.com>
2023-03-27 20:07:44 -0300

    Pushing CRUD into repository

---
+++ database/migrations/2023_03_24_172011_create_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); 
+            $table->string('phone');
+            $table->string('description');
+            $table->boolean('status'); // Matrículado ou transferido
+            $table->string('grade')->nullable(); // Se estiver matrículado, not null, se não, null.
+            $table->text('file')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migratimage.pngions.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
---
+++ resources/views/register_students.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CADASTRAR | WANDERER</title>
+</head>
+<body>
+    <h1>CADASTRAR ALUNO</h1>
+
+    <label for="">Nome: </label>
+    <input name="name" type="text" placeholder="Digite o nome...">
+
+    <br /><br />
+
+    <label for="">Celular: </label>
+    <input name="phone" type="text" placeholder="Digite o celular...">
+
+    <br /><br />
+
+    <label for="">Descrição: </label>
+    <input name="description" type="textarea" placeholder="Digite a descrição...">
+
+    <br /><br />
+
+    <label for="">Status: </label>
+
+    <select name="status" id="cars">
+        <option value=0> ======== </option>
+        <option value=0>Matrículado</option>
+        <option value=1>Transferido</option>
+    </select>
+
+    <br /><br />
+
+    <label for="">Série: </label>
+    <input name="status" type="text" placeholder="Digite a série do aluno...">
+
+    <br /><br />
+
+    <label for="">Inserir foto do aluno: </label>
+    <input name="file" type="file" placeholder="Insira a foto do aluno">
+</body>
+</html>
