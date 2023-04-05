23f0db3232c76c3371c5c71cca2622dee3171291
leoc0104 <leocamargo0104@gmail.com>
2023-04-05 11:05:39 -0300

    Creating a relation method with student model

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function relStudents(){
+        return $this->hasMany('App\Models\ModelStudent', 'user_id');
+    }
 }
