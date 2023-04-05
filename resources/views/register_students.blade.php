dd1eb7938290e80a7793bd77d180b199bf07523d
leoc0104 <leocamargo0104@gmail.com>
2023-04-04 21:40:21 -0300

    Editing register view

--- resources/views/register_students.blade.php
+++ resources/views/register_students.blade.php
@@ -7,47 +7,5 @@
     <title>CADASTRAR</title>
 </head>
 <body>
-    <h1>CADASTRAR ALUNO</h1>
-
-    <form action="/register_students" method="POST">
-        @csrf
-
-        <label for="">Nome: </label>
-        <input name="name" type="text" placeholder="Digite o nome...">
-
-        <br /><br />
-
-        <label for="">Celular: </label>
-        <input name="phone" type="text" placeholder="Digite o celular...">
-
-        <br /><br />
-
-        <label for="">Descrição: </label>
-        <input name="description" type="text" placeholder="Digite a descrição...">
-
-        <br /><br />
-
-        <label for="">Status: </label>
-
-        <select name="status">
-            <option value="null"> ========== </option>
-            <option value="0">Matrículado</option>
-            <option value="1">Transferido</option>
-        </select>
-
-        <br /><br />
-
-        <label for="">Série: </label>
-        <input name="status" type="text" placeholder="Digite a série do aluno...">
-
-        <br /><br />
-
-        <label for="">Inserir foto do aluno: </label>
-        <input name="file" type="file" placeholder="Insira a foto do aluno">
-
-        <br /><br /><br /><br />
-
-        <button>SALVAR</button>
-    </form>
 </body>
 </html>
