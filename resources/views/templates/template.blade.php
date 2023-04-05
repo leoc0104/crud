626872c40bf6034a3ca6109db2a2c562cdb4493e
leoc0104 <leocamargo0104@gmail.com>
2023-04-05 10:50:00 -0300

    Deleting body from template

--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -9,34 +9,5 @@
 </head>
 <body>
 	@yield('content')
-
-	<h2>LISTA DE ALUNOS</h2>
-
-    <table>
-		<tr>
-			<th>Foto</th>
-			<th>ID</th>
-			<th>Nome</th>
-			<th>Telefone</th>
-			<th>Descrição</th>
-			<th>Status</th>
-			<th>Série</th>
-			<th>Ação</th>
-		</tr>	
-		<tr>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td><a href=>[Excluir]</a><a href=''>[Alterar]</a></td>
-		</tr>
-	</table>
-
-	<button>ADICIONAR</button>
-
-	<button>PESQUISAR</button>
 </body>
 </html>
