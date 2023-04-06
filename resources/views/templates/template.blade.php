96c980c2807a5d75f5ded2382c9c589cec628417
leoc0104 <leocamargo0104@gmail.com>
2023-04-06 09:48:55 -0300

    Linking favicon

--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" href="{{url('assets/bootstrap/css/bootstrap.min.css')}}">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <title>CRUD</title>
 </head>
 <body>
