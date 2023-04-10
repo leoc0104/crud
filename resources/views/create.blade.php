5d8ae74f794b86045592910eee5cf14de397d226
leoc0104 <leocamargo0104@gmail.com>
2023-04-10 16:34:28 -0300

    Inserting into inputs 'required' to validations of fields

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -7,16 +7,16 @@
     <form name="formCre" id="formCre" method="POST" action="{{url('students')}}">
       @csrf
 
-      <input class="form-control" type="text" name="nickname" id="nickname" placeholder="Apelido"> <br />
-      <select class="form-control" name="user_id" id="user_id">
+      <input class="form-control" type="text" name="nickname" id="nickname" placeholder="Apelido" required> <br />
+      <select class="form-control" name="user_id" id="user_id" required>
         <option value="">--Selecione o usuário--</option>
         @foreach ($users as $user)
             <option value="{{$user->id}}">{{$user->name}}</option>
         @endforeach
       </select>
       <br />
-      <input class="form-control" type="text" name="age" id="age" placeholder="Idade"> <br />
-      <input class="form-control" type="text" name="height" id="height" placeholder="Altura"> <br />
+      <input class="form-control" type="text" name="age" id="age" placeholder="Idade" required> <br />
+      <input class="form-control" type="text" name="height" id="height" placeholder="Altura" required> <br />
       <input class="btn btn-primary" type="submit" value="CADASTRAR">
     </form>
   </div>
