672a2b79d687f10cf9ae1b6d3d5590971898be88
leoc0104 <leocamargo0104@gmail.com>
2023-04-10 15:00:13 -0300

    Adding view to register a new data

---
+++ resources/views/create.blade.php
@@ -0,0 +1,23 @@
+@extends('templates.template')
+
+@section('content')
+  <h1 class="text-center mt-5 mb-4">CADASTRAR</h1>
+
+  <div class="col-8 m-auto">
+    <form name="formCre" id="formCre" method="POST" action="{{url('students')}}">
+      @csrf
+
+      <input class="form-control" type="text" name="nickname" id="nickname" placeholder="Apelido"> <br />
+      <select class="form-control" name="user_id" id="user_id">
+        <option value="">Selecione o usuário</option>
+        @foreach ($users as $user)
+            <option value="{{$user->id}}">{{$user->name}}</option>
+        @endforeach
+      </select>
+      <br />
+      <input class="form-control" type="text" name="age" id="age" placeholder="Idade"> <br />
+      <input class="form-control" type="text" name="height" id="height" placeholder="Altura"> <br />
+      <input class="btn btn-primary" type="submit" value="CADASTRAR">
+    </form>
+  </div>
+@endsection
