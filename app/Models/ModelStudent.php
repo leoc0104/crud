87459439156175872c0030df3972fe724e8f007f
leoc0104 <leocamargo0104@gmail.com>
2023-04-10 16:10:14 -0300

    Adding a security incrementy fillable

--- app/Models/ModelStudent.php
+++ app/Models/ModelStudent.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class ModelStudent extends Model
 {
     protected $table = 'students';
+    protected $fillable = ['nickname', 'user_id', 'age', 'height'];
 
     public function relUsers(){
         return $this->hasOne('App\Models\User', 'id', 'user_id');
